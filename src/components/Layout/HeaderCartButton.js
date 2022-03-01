1b6e2eeda2ccfdb018863410270c244fb0244be8
Goran Mitkovski <37515429+cyberVandal@users.noreply.github.com>
2022-03-01 21:32:00 +0100

    Adding Header and Buttons

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react/cjs/react.production.min"
+
+import classes from './HeaderCartButton.module.css'
+
+import CartIcon from '../Cart/CartIcon';
+
+
+const HeaderCartButton = props => {
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>
+            6
+        </span>
+    </button>
+
+
+};
+
+export default HeaderCartButton;
